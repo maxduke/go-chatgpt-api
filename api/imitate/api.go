1648462668531ec58b18ebee8ad60ada34c2f13b
maxduke <maxduke@gmail.com>
2024-05-28 06:36:39 +0000

    try to fix http 422 error

--- api/imitate/api.go
+++ api/imitate/api.go
@@ -28,6 +28,7 @@ import (
 var (
 	reg *regexp.Regexp
 	token string
+	gptsRegexp = regexp.MustCompile(`-gizmo-g-(\w+)`)
 )
 
 func init() {
@@ -220,21 +221,17 @@ func convertAPIRequest(api_request APIRequest) (chatgpt.CreateConversationReques
 	if strings.HasPrefix(api_request.Model, "gpt-3.5") {
 		chatgpt_request.Model = "text-davinci-002-render-sha"
 	} else if strings.HasPrefix(api_request.Model, "gpt-4") {
-		if api_request.Model == "gpt-4o" {
-			chatgpt_request.Model = api_request.Model
-		} else {
-			chatgpt_request.Model = "gpt-4"
-		}		
-		// TODO support gpts
-		// if len(api_request.Model) > 12 {
-		// 	key := api_request.Model[6:11]
-		// 	if key == "gizmo" {
-		// 		val := api_request.Model[12:]
-		// 		chatgpt_request.ConversationMode.Kind = "gizmo_interaction"
-		// 		chatgpt_request.ConversationMode.GizmoId = val
-		// 	}
-		// }
+		chatgpt_request.Model = "gpt-4"
+		if strings.HasPrefix(api_request.Model, "gpt-4o") {
+			chatgpt_request.Model = "gpt-4o"
+		}
 	}
+	matches := gptsRegexp.FindStringSubmatch(api_request.Model)
+	if len(matches) == 2 {
+		chatgpt_request.ConversationMode.Kind = "gizmo_interaction"
+		chatgpt_request.ConversationMode.GizmoId = "g-" + matches[1]
+	}
+
 	for _, api_message := range api_request.Messages {
 		if api_message.Role == "system" {
 			api_message.Role = "critic"
@@ -252,6 +249,7 @@ func NewChatGPTRequest() chatgpt.CreateConversationRequest {
 		Model:                      "text-davinci-002-render-sha",
 		ForceUseSse:                true,
 		HistoryAndTrainingDisabled: disable_history,
+		ConversationMode:           chatgpt.ConvMode{Kind: "primary_assistant"},
 		WebsocketRequestId:         uuid.NewString(),
 	}
 }
